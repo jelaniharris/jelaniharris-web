46a219798bc5eda5cad101c5c17fb3023cd3f186
Jelani Harris <mayanobsidian@gmail.com>
2022-09-15 14:54:33 -0500

    Directly refer to the image on homepage and leverage WebPack

--- src/components/home/hero-me.js
+++ src/components/home/hero-me.js
@@ -1,13 +1,12 @@
 import * as React from "react"
 import SocialIcons from "../social-icons";
-import { StaticImage } from "gatsby-plugin-image"
+import HeroLogo from "../../images/hero_logo.png"
 
 const HeroMe = () => {
     return (
         <section className="hero is-large has-background front-hero-me">
-          <StaticImage src="../../images/hero_logo.png" className="hero-background is-transparent" layout="fullWidth" alt="" style={{gridArea: "1/1"}} />
-        <div className="hero-body">
-          
+          <img src={HeroLogo} className="hero-background is-transparent" />
+          <div className="hero-body">
           <div className="container has-text-centered">
           <h1 className="title is-1">Hi, I'm <span className="has-text-weight-bold">Jelani</span></h1>
           <p className="subtitle">I'm a Senior Full Stack Software Engineer</p>
