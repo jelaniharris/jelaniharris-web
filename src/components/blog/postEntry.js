b5eb6ba5ee1a28397d4b918d28600f8fb749bba5
Jelani Harris <407760+jelaniharris@users.noreply.github.com>
2025-01-14 20:16:10 -0600

    Added support for contentful data for post entries in blog list

--- src/components/blog/postEntry.js
+++ src/components/blog/postEntry.js
@@ -5,13 +5,13 @@ import ShowTags from "../common/showTags"
 
 const PostEntry = ({ post }) => {
   const { frontmatter, fields, excerpt } = post
-  let featuredImgFixed = getImage(frontmatter.featuredImage)
+  let featuredImgFixed = getImage(post.featuredImage ?? frontmatter.featuredImage)
 
   return (
-    <div className={`card my-3 ${frontmatter.draft ? "blog-card-draft" : ""}`}>
+    <div className={`card my-3 ${frontmatter && frontmatter.draft ? "blog-card-draft" : ""}`}>
       <div className="is-flex is-flex-direction-row is-align-content-flex-start">
         <figure>
-          <Link to={fields.slug}>
+          <Link to={post.slug ?? fields.slug}>
             <GatsbyImage
               image={featuredImgFixed}
               width={400}
@@ -24,14 +24,14 @@ const PostEntry = ({ post }) => {
           style={{ width: "100%" }}
         >
           <div>
-            <Link to={fields.slug}>
-              <h3 className="title is-3">{frontmatter.title}</h3>
+            <Link to={post.slug ?? fields.slug}>
+              <h3 className="title is-3">{post.title ?? frontmatter.title}</h3>
             </Link>
-            <p className="mt-3">{frontmatter.description || excerpt}</p>
+            <p className="mt-3">{post?.description?.description || frontmatter.description || excerpt}</p>
           </div>
           <div className="is-flex is-justify-content-space-between mt-3">
-            <time dateTime={frontmatter.formatdate}>{frontmatter.date}</time>
-            <ShowTags tags={frontmatter.tags} hideLabel />
+            <time dateTime={post.formatdate ?? frontmatter.formatdate}>{post.date ?? frontmatter.date}</time>
+            <ShowTags tags={post.tags ?? frontmatter.tags} hideLabel />
           </div>
         </div>
       </div>
